96e0e57cb2a0e2c88d5ccc67ddfee7380cfb8b10
Enrique Villa <iggyvilla@icloud.com>
2026-04-11 17:45:52 +0800

    fixed server

--- Assets/Scripts/SocketServer/SocketIOServer.cs
+++ Assets/Scripts/SocketServer/SocketIOServer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using SocketIOClient;
 using SocketIOClient.Newtonsoft.Json;
@@ -18,7 +19,8 @@ public class SocketIOServer : MonoBehaviour
     public int serverPort = 6060;
 
     public SocketIOUnity socket;
-
+    
+    public GameObject debugCanvas;
     public GameObject agentGameObject;
 
     private bool isWalkingToPath = false;
@@ -37,25 +39,40 @@ public class SocketIOServer : MonoBehaviour
         {
             Query = new Dictionary<string, string>
             {
-                {"token", "UNITY" }
+                {"sari_instance", "UNITY" }
             },
             EIO = EngineIO.V4,
             Transport = SocketIOClient.Transport.TransportProtocol.WebSocket
         });
         
         socket.JsonSerializer = new NewtonsoftJsonSerializer();
+        socket.unityThreadScope = SocketIOUnity.UnityThreadScope.Update; 
         
         socket.OnConnected += (sender, e) =>
         {
             ServerLog("Connected to SocketIO server.");
         };
         
-        socket.unityThreadScope = SocketIOUnity.UnityThreadScope.Update; 
         socket.OnUnityThread("MOVE_FWD", (data) =>
         {
             float amount = data.GetValue<float>();
             ServerLog($"recv MOVE_FWD({amount})");
             agentGameObject.transform.position += agentGameObject.transform.forward * amount;
+            StartCoroutine(GetScreenshotBase64((base64) =>
+                {
+                    socket.Emit("UNITY_RESPONSE", base64);
+                })
+            );
+        });
+        
+        socket.OnUnityThread("GET_VIEW", (data) =>
+        {
+            ServerLog($"recv GET_VIEW()");
+            StartCoroutine(GetScreenshotBase64((base64) =>
+                {
+                    socket.Emit("UNITY_RESPONSE", base64);
+                })
+            );
         });
         
         socket.OnUnityThread("SARI_AGENT_UPDATE_PATH", (data) =>
@@ -99,6 +116,31 @@ public class SocketIOServer : MonoBehaviour
                 }
             }
         }
+    }
+    
+    public IEnumerator GetScreenshotBase64(Action<string> callback)
+    {
+        // 1. Wait until the end of the frame 
+        // This ensures all draw calls (including procedural ones) are finished
+        yield return new WaitForEndOfFrame();
+        
+        /* Due to Unity's asynchronous nature, the render might capture the
+         * movement BEFORE a movement command. We want it to capture AFTER,
+         * hence this delay */
+        yield return new WaitForSeconds(0.5f);
+
+        // 2. Capture the current screen buffer
+        Texture2D screenShot = ScreenCapture.CaptureScreenshotAsTexture();
+
+        // 3. Convert to Base64
+        byte[] bytes = screenShot.EncodeToPNG();
+        string base64String = Convert.ToBase64String(bytes);
+
+        // 4. Clean up memory
+        Destroy(screenShot);
+
+        // 5. Return result via callback
+        callback?.Invoke(base64String);
     }
 
     void ServerLog(string msg)
